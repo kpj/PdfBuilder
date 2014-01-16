045e9c5f970cdf48e55f1d3ac7ce2c6b15a34f55
kpj <kpjkpjkpjkpjkpjkpj@googlemail.com>
2014-01-16 23:29:18 +0100

    Added hooks

---
+++ hooks/git_handler.py
@@ -0,0 +1,28 @@
+import socketserver, json, subprocess
+import http.server, urllib.parse
+
+
+# latex variables
+compile_substring = "main" # compile .tex file if this substring is present
+
+# server variables
+port = 8080
+
+# helper functions
+def handle_git_request(req):
+	for commit in req["commits"]:
+		stuff = commit["modified"] + commit["added"]
+		for entry in [e for e in stuff if compile_substring in e]:
+			subprocess.call(["./latex_maker.sh", entry])
+
+def get_data(sock, con_len):
+	return urllib.parse.unquote(sock.recv(con_len).decode(encoding="UTF-8"))
+
+class RequestHandler(http.server.CGIHTTPRequestHandler):
+	def do_POST(self):
+		req = json.loads(get_data(self.request, int(self.headers["Content-Length"]))[8:])
+		handle_git_request(req)
+
+# start server
+httpd = socketserver.TCPServer(("0.0.0.0", port), RequestHandler)
+httpd.serve_forever()
---
+++ hooks/latex_maker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+pdflatex_cmd="pdflatex -halt-on-error -interaction nonstopmode"
+bibtex_cmd="bibtex"
+
+file=$(basename "$1")
+dir=$(dirname "$1")
+pdf="${file%.tex}.pdf";
+echo "Compiling \"$file\" in \"$dir\""
+
+cd /tmp
+rm -fr PdfBuilder
+git clone git@github.com:kpj/PdfBuilder.git
+cd PdfBuilder
+
+cd "$dir"
+$pdflatex_cmd "$file"
+$bibtex_cmd "$file"
+$pdflatex_cmd "$file"
+$pdflatex_cmd "$file"
+cd -
+
+git add "$pdf"
+git commit -m "Compiled $file to $pdf" "$pdf"
+git push
+
+cd ..
+rm -fr PdfBuilder
